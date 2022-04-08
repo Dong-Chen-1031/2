de949ac3decc3196bb8eec2d052153dc534bda15
Dong-Chen-1031 <90507781+Dong-Chen-1031@users.noreply.github.com>
2022-04-08 16:08:57 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,20 @@
 radio.onReceivedValue(function (name, value) {
-    basic.showIcon(IconNames.Yes)
     if (name == "ture") {
-        pins.servoWritePin(AnalogPin.P1, 90)
-        pins.servoWritePin(AnalogPin.P2, 0)
-        basic.pause(2000)
+        basic.showIcon(IconNames.Yes)
         pins.servoWritePin(AnalogPin.P1, 0)
-        pins.servoWritePin(AnalogPin.P2, 90)
+        pins.servoWritePin(AnalogPin.P2, 180)
         basic.showNumber(value)
     } else if (name == "false") {
-    	
+        pins.servoWritePin(AnalogPin.P1, 90)
+        pins.servoWritePin(AnalogPin.P2, 90)
     }
+    basic.clearScreen()
 })
+pins.servoWritePin(AnalogPin.P1, 90)
+pins.servoWritePin(AnalogPin.P2, 90)
 radio.setGroup(4)
 basic.showIcon(IconNames.Yes)
-basic.pause(1000)
+basic.pause(5000)
 basic.clearScreen()
 basic.forever(function () {
 	
